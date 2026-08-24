004dd0c0ec3c59cdefdecfcdaaaba35187d1e61f
mohamedyassin-source <mohamed.yassin@almarasem.com>
2026-08-24 23:01:28 +0300

    Update EditEmployeeModal.tsx

--- components/modals/EditEmployeeModal.tsx
+++ components/modals/EditEmployeeModal.tsx
@@ -1,5 +1,6 @@
 import { supabase } from '@/lib/supabase';
-
+import ModalShell from '@/components/ModalShell';
+import Field from '@/components/Field';
 export interface EditData {
   emp: any;
   contract: any;
