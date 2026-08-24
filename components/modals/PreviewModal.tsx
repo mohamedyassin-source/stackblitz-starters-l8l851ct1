5f172a7e8b7bf42fdbcca416db17259a4941a3d3
mohamedyassin-source <mohamed.yassin@almarasem.com>
2026-08-24 23:02:37 +0300

    Update PreviewModal.tsx

--- components/modals/PreviewModal.tsx
+++ components/modals/PreviewModal.tsx
@@ -1,6 +1,6 @@
 import { supabase } from '@/lib/supabase';
 import ModalShell from '@/components/ModalShell';
-import Field from '@/components/Field';
+import Stamp from '@/components/Stamp';
 export interface PreviewData {
   emp: any;
   contracts: any[];
